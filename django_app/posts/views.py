c5ddff00b8d02788c6992e5e688ae5ac11cbbae7
Andika Nugraha <m.andika.nugraha@gmail.com>
2018-12-25 22:55:14 +0700

    crarsh course

--- django_app/posts/views.py
+++ django_app/posts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Posts
 
 # Create your views here.
+def index(request):
+
+    posts = Posts.objects.all()[:10]
+
+    context = {
+        'title' : 'Lates posts',
+        'posts' : posts
+    }
+
+    # return HttpResponse('Hello from post')
+    # return render(request,'posts/index.html', {
+    #     'name': 'Andika Nugraha'
+    # })
+    return render(request,'posts/index.html',context)
+
+def details(request,id):
+    post = Posts.objects.get(id=id)
+
+    context = {
+        'post' : post
+    }
+
+    return render(request,'posts/details.html',context)
