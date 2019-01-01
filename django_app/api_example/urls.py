31818cd4d8ec0bf9dee3a805d86e313e03429a26
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2019-01-01 22:50:10 +0700

    costum action

--- django_app/api_example/urls.py
+++ django_app/api_example/urls.py
@@ -3,7 +3,10 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('api_example',views.Api_exampleViewSet)
+router.register('api_example',views.Api_exampleViewSet, base_name='example-api')
+
+# for url in router.urls:
+#     print(url, '\n')
 
 urlpatterns = [
     path('', include(router.urls))
--- django_app/api_example/views.py
+++ django_app/api_example/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from .models import ApiExample
 from .serializers import Api_exampleSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+# class Api_exampleViewSet(viewsets.ViewSet):
+#       #List only
+#     def list(self, request):
+#         queryset = ApiExample.objects.all()
+#         serializer = Api_exampleSerializer(queryset, many=True)
+#         return Response(serializer.data)
 
 class Api_exampleViewSet(viewsets.ModelViewSet):
+
+    # list, create, retrieve, update, partial_update, destroy
+
     queryset = ApiExample.objects.all()
     serializer_class = Api_exampleSerializer
-# Create your views here.
+
+    @action(methods=['GET'], detail=False)
+    def newest(self,request):
+        newest = self.get_queryset().order_by('created').last()
+        serializer = self.get_serializer_class()(newest)
+        return Response(serializer.data)
+
